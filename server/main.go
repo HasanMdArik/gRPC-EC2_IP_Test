3eefbe5fb9d24e8fb9e138b00e4bb0266b4f0d69
Mohammad <mohammadarik63@gmail.com>
2022-06-27 08:23:18 +0600

    server-side code done

--- server/main.go
+++ server/main.go
@@ -1,10 +1,34 @@
 package main
 
-import "log"
+import (
+	"ec2-grpc-ip-test/server/IPTestService"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func panicOnErr(err error) {
+	if err != nil {
+		log.Println("Error:", err.Error())
+		panic(err)
+	}
+}
 
 func main() {
 	log.Println("IP test server")
 	// Listener for server
+	listener, err := net.Listen("tcp", ":2500")
+	panicOnErr(err)
 	// Initializing the handler
+	serverHandler := &IPTestServiceHandler{}
+
+	// Initializing the gRPC server
+	opts := []grpc.ServerOption{}
+	grpcServer := grpc.NewServer(opts...)
+
+	IPTestService.RegisterIP_TestServer(grpcServer, serverHandler)
 	// Serving the server
+	err = grpcServer.Serve(listener)
+	panicOnErr(err)
 }
